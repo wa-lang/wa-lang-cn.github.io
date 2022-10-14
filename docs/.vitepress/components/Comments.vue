1f44f20d9930408a8d08f7b92c606d353f73e413
xxx <humengmingxx@gmail.com>
2022-10-14 23:40:01 +0800

    feat：support comments

---
+++ docs/.vitepress/components/Comments.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import Giscus from '@giscus/vue';
+</script>
+
+<template>
+  <div class="comments">
+    <Giscus
+      repo="wa-lang/wa-lang.github.io"
+      repoId="MDEwOlJlcG9zaXRvcnkxNjI5NDU2MDU="
+      category="General"
+      categoryId="DIC_kwDOCbZaRc4CQYyU"
+      mapping="pathname"
+      term="Welcome to wa!"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="preferred_color_scheme"
+      lang="zh-CN"
+      loading="lazy"
+    />
+  </div>
+</template>
+
+<style scoped>
+.comments {
+  margin-top: 2em
+}
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,12 +3,14 @@ import Theme from 'vitepress/theme'
 import '../styles/vars.css'
 import HomePage from '../components/HomePage.vue'
 import SvgImage from '../components/SvgImage.vue'
+import Comments from '../components/Comments.vue'
 
 export default {
   ...Theme,
   Layout() {
     return h(Theme.Layout, null, {
       'home-features-after': () => h(HomePage),
+      'doc-after': () => h(Comments)
     })
   },
   enhanceApp({ app }) {
