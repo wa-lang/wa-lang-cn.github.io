398906830becbb1ea8ca10aa013d28da1bd09b0b
xxx <humengmingxx@gmail.com>
2023-07-22 19:13:41 +0800

    Revert "zz"
    This reverts commit ae4b157343d6a48467ab7aaeeaebce5ae36ca37b.

--- docs/.vitepress/inlined-scripts/redirectDocsPath.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  const pathname = window.location.pathname
-  if ([
-    '/reference/spec.html',
-    '/reference/design-wz.html',
-  ].includes(pathname)) {
-    window.location.pathname = pathname.replace('/reference/', '/guide/')
-  }
-})()
