2da76a81d71336d3bb5ac969190ebc2fbf219537
chai2010 <chaishushan@gmail.com>
2024-11-06 10:55:15 +0800

    zz

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -40,6 +40,7 @@ export default async () => defineConfig({
     ],
 
     socialLinks: [
+      { icon: { svg: ICONS.gitcode }, link: 'https://gitcode.com/wa-lang/wa' },
       { icon: { svg: ICONS.gitee }, link: 'https://gitee.com/wa-lang/wa' },
       { icon: 'github', link: 'https://github.com/wa-lang/wa/' },
     ],
