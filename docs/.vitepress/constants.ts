7717587eb5e2d2e64eff5f26dd8fbf569af3257d
chai2010 <chaishushan@gmail.com>
2025-10-27 07:19:06 +0800

    zz

--- docs/.vitepress/constants.ts
+++ docs/.vitepress/constants.ts
@@ -45,6 +45,7 @@ export const DEMOS = [
   { name: 'Playground', logoName: 'wa.png', link: 'https://wa-lang.org/playground/' },
   { name: '贪吃蛇', logoName: 'wa.png', link: 'https://wa-lang.org/wa/snake/' },
   { name: '贪吃蛇(WASM4)', logoName: 'wa.png', link: 'https://wa-lang.org/wa/w4-snake/' },
+  { name: '贪吃蛇(凹中文实现)', logoName: 'wa.png', link: 'https://wa-lang.org/wa/w4-snake-wz/' },
   { name: '生命游戏(WASM4)', logoName: 'wa.png', link: 'https://wa-lang.org/wa/w4-life/' },
   { name: '2048游戏', logoName: 'wa.png', link: 'https://wa-lang.org/wa/w4-2048/' },
   { name: '俄罗斯方块', logoName: 'wa.png', link: 'https://wa-lang.org/wa/w4-tetris/' },
